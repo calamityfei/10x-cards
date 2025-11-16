0342cd35a21736e7b80f7ef3d0820efe874a32aa
Krzysztof Chrustowski <calamity.fei@gmail.com>
2025-11-16 22:24:59 +0100

    fix: fix login page pom first login issue

--- tests/e2e/pages/LoginPage.ts
+++ tests/e2e/pages/LoginPage.ts
@@ -8,10 +8,23 @@ export class LoginPage {
   }
 
   async login(email: string, password: string) {
-    await this.page.locator("#email").fill(email);
-    await this.page.locator("#password").fill(password);
-    await this.page.getByTestId("login-submit-button").click();
     await this.page.waitForLoadState("networkidle");
+    const emailInput = this.page.locator("#email");
+    const passwordInput = this.page.locator("#password");
+    const submitButton = this.page.getByTestId("login-submit-button");
+
+    await emailInput.waitFor({ state: "visible" });
+    await emailInput.click();
+    await emailInput.fill(email);
+
+    await passwordInput.click();
+    await passwordInput.fill(password);
+
+    await Promise.all([
+      this.page.waitForResponse((response) => response.url().includes("/api/auth/login") && response.status() === 200),
+      submitButton.click(),
+    ]);
+    await this.page.waitForURL("/my-flashcards", { timeout: 15000 });
   }
 
   async getErrorMessage() {
